1d82b5dcce05a996e41ea82a1a2a0e7e9a9587dc
Joao Antonio <joaopsilva.dev@gmail.com>
2022-12-16 16:16:38 -0300

    feat: :construction: requisitos 3.1, 3.2, 3.3 e 4.1 completos
    implementando método de adicionar pedidos e verificar estoque

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -16,10 +16,59 @@ class InventoryControl:
     }
 
     def __init__(self):
-        pass
+        self._data = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0,
+        }
 
     def add_new_order(self, customer, order, day):
-        pass
+        if order == "hamburguer":
+            if (
+                self._data["pao"] < 50 and
+                self._data["carne"] < 50 and
+                self._data["queijo"] < 100
+            ):
+                self._data["pao"] += 1
+                self._data["carne"] += 1
+                self._data["queijo"] += 1
+            else:
+                return False
+        elif order == "pizza":
+            if (
+                self._data["massa"] < 50 and
+                self._data["molho"] < 50 and
+                self._data["queijo"] < 100
+            ):
+                self._data["massa"] += 1
+                self._data["queijo"] += 1
+                self._data["molho"] += 1
+            else:
+                return False
+        elif order == "misto-quente":
+            if (
+                self._data["pao"] < 50 and
+                self._data["presunto"] < 50 and
+                self._data["queijo"] < 100
+            ):
+                self._data["pao"] += 1
+                self._data["presunto"] += 1
+                self._data["queijo"] += 1
+            else:
+                return False
+        elif order == "coxinha":
+            if (
+                self._data["massa"] < 50 and
+                self._data["frango"] < 50
+            ):
+                self._data["massa"] += 1
+                self._data["frango"] += 1
+            else:
+                return False
 
     def get_quantities_to_buy(self):
-        pass
+        return self._data
